aec564f3045816871c7441245a34a5ba8fcf8137
bahlool-work <bahlool.ramesh70720@gmail.com>
2024-07-07 17:01:27 +0330

    first push (Requirements)

---
+++ nuxt-app/pages/[...slug].vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>number: {{ $route.params.slug }}</h1> 
+        <NuxtLink to="/" target="_blank">click</NuxtLink>
+    </div>
+</template>
---
+++ nuxt-app/pages/user-[group]/[id].vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>number: {{ $route.params.group }}</h1> 
+        <h1>number: {{ $route.params.id }}>>>>>>>>>>welcome</h1> 
+    </div>
+</template>
