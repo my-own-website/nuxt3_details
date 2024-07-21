4ad65105beab2f9f6880bb1b5a2db09ad9b7c5ab
bahlool1401 <bahlool.ramesh@gmail.com>
2024-07-21 23:59:22 +0330

    add preview and main page for shopping

---
+++ nuxt-app/components/products/preview.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="max-w-sm bg-white rounded-lg border border-indigo-200 shadow-md dark:bg-gray-800">
+        <nuxt-linl :to="`product/${Product.slug}`">
+
+        </nuxt-linl>
+    </div>
+</template>
---
+++ nuxt-app/pages/shopping.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="flex justify-around mt-20" v-if="products">
+            <div v-for="product in products" :key="product.id">
+                <ProductsPreview :product="product"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import {useProductsStore} from '../stores/products'
+    const productsStore = useProductsStore()
+    const products = computed(()=>{
+        return productsStore.products
+    })
+</script>
